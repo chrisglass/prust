adbb4fd82eed90224e024f59b3e26c038dd7e7ab
Chris Glass <tribaal@ubuntu.com>
2020-11-28 17:32:34 +0100

    WIP

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+extern crate handlebars;
+extern crate hyper;
+extern crate serde_json;
+extern crate sqlite;
+
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+fn get_index(req: Request<Body>) -> Body {
+    Body::from("Try posting data to /echo")
+}
+
+async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            *response.body_mut() = get_index(req);
+        }
+        (&Method::GET, "/echo") => {
+            *response.body_mut() = Body::from("Blah");
+        }
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    }
+
+    Ok(response)
+}
+
+#[tokio::main]
+async fn main() {
+    // We'll bind to 127.0.0.1:3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
